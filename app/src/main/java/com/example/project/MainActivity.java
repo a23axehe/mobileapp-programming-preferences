3b532ede42cddac674fb40a48811edf2b88a22bb
a23axehe <a23axehe@student.his.se>
2024-05-03 09:36:12 +0200

    created mainactivity with intents from previous assignments and read preferences constructor from canvas guide, one of the first steps I did but im afraid I did not commit it correctly

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    // in My MainActivity class
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        // In onCreate()
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+
+
+// Read a preference
+        TextView prefTextRef = new TextView(this);
+        prefTextRef = (TextView) findViewById(R.id.textview1);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+
+        // Define the button and set its onClickListener inside onCreate method
+        Button secondActivityButton = findViewById(R.id.button);
+        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the second activity
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
